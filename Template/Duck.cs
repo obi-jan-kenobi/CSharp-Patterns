80b71701842f5750668b7d996d63dd359c5b8650
Jan Sziegaud <j.sziegaud@gmail.com>
2016-07-27 10:54:02 +0200

    template

---
+++ Template/Duck.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Template {
+    class Duck : IComparable {
+        string name;
+        int weight;
+
+        public Duck(string name, int weight) {
+            this.name = name;
+            this.weight = weight;
+        }
+
+        public override string ToString() {
+            return name + ": " + weight;
+        }
+
+        public int CompareTo(object obj) {
+            Duck otherDuck = obj as Duck;
+            if (otherDuck == null) {
+                return 0;
+            }
+
+            if (this.weight < otherDuck.weight) {
+                return -1;
+            }
+            else if (this.weight > otherDuck.weight) {
+                return 1;
+            }
+            else {
+                return 0;
+            }
+        }
+    }
+}
---
+++ Template/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Template {
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+
+        public MainWindow() {
+
+            Duck[] ducks = {
+                new Duck("Daffy", 8),
+                new Duck("Doofy", 2),
+                new Duck("Howard", 3),
+                new Duck("Tick", 7),
+                new Duck("Trick", 6),
+                new Duck("Tack", 7)
+            };
+
+
+            InitializeComponent();
+            
+            Console.WriteLine("Before sorting:");
+            display(ducks);
+
+            Array.Sort(ducks);
+
+            Console.WriteLine("after sorting:");
+            display(ducks);
+        }
+
+        internal static void display(Duck[] ducks) {
+            foreach (Duck duck in ducks) {
+                Console.WriteLine(duck);
+            }
+        }
+    }
+}
